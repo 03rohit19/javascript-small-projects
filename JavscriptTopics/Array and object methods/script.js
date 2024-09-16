4d5995eb93deb808a7092aff6d9407e9c163cb26
Rohit <rohitfriday01@gmail.com>
2024-09-16 13:01:46 +0530

    Adding files

---
+++ JavscriptTopics/Array and object methods/script.js
@@ -0,0 +1,18 @@
+const fruits = ["apple", "banana", "mango", "orange"];
+fruits.forEach((fruit) => {
+  console.log(fruit);
+});
+
+const students = [
+  { name: "Rohit", age: 18 },
+  { name: "Ro", age: 19 },
+  { name: "Rohi", age: 25 },
+  { name: "Rot", age: 11 },
+];
+
+const adultStudents = students.filter((student) => {
+  return student.age > 18;
+});
+const adultStudentName = adultStudents.map((stu) => {
+  return stu.name;
+});
